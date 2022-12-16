a8e574853afc4a564ee9aded2217527d90a9e7a2
AynSiddiqui <ayaansiddiqui68@gmail.com>
2022-12-16 16:56:47 +0530

    Login and Register updated
    Added flash messages and errors

--- Login/app.js
+++ Login/app.js
@@ -4,9 +4,12 @@ const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const session = require('express-session');
 const flash = require('connect-flash');
+const ExpressError = require('../utils/ExpressError');
 const methodOverride = require('method-override');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
+const catchAsync = require('../utils/catchAsync');
+const User = require('./models/user');
 
 mongoose.set("strictQuery", true);
 mongoose.connect('mongodb://127.0.0.1:27017/mypharmacy', {
@@ -47,10 +50,10 @@ app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 
-// passport.use(new LocalStrategy(User.authenticate()));
+passport.use(new LocalStrategy(User.authenticate()));
 
-// passport.serializeUser(User.serializeUser());
-// passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
     console.log(req.session)
@@ -60,16 +63,19 @@ app.use((req, res, next) => {
     next();
 })
 
+app.get('/', (req, res) => {
+    res.render('home');
+})
 
 app.get('/login', (req, res) => {
     res.render('users/login');
 })
 
-app.post('/login', (req, res) => {
-    // req.flash('success', 'welcome back!');
-    // const redirectUrl = req.session.returnTo || '/campgrounds';
-    // delete req.session.returnTo;
-    // res.redirect(redirectUrl);
+app.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    req.flash('success', 'welcome back!');
+    const redirectUrl = req.session.returnTo || '/login';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
     res.send(req.body)
 })
 
@@ -77,30 +83,38 @@ app.get('/register', (req, res) => {
     res.render('users/register');
 })
 
-app.post('/register', (req, res) => {
-    // req.flash('success', 'welcome back!');
-    // const redirectUrl = req.session.returnTo || '/campgrounds';
-    // delete req.session.returnTo;
-    // res.redirect(redirectUrl);
-    res.send(req.body)
-})
+app.post('/register', catchAsync(async (req, res, next) => {
+    try {
+        const {fullname, email, username, password } = req.body;
+        const user = new User({fullname, email, username });
+        const registeredUser = await User.register(user, password);
+        req.login(registeredUser, err => {
+            if (err) return next(err);
+            req.flash('success', 'Welcome!');
+            res.redirect('/');
+        })
+    } catch (e) {
+        req.flash('error', e.message);
+        res.redirect('register');
+    }
+}));
 
 app.get('/logout', (req, res) => {
     req.logout();
     req.flash('success', "Goodbye!");
-    res.redirect('/campgrounds');
+    // res.redirect('/campgrounds');
 })
 
 app.all('*', (req, res, next) => {
     res.send("Page not found");
-    // next(new ExpressError('Page Not Found', 404))
+    next(new ExpressError('Page Not Found', 404))
 })
 
-// app.use((err, req, res, next) => {
-//     const { statusCode = 500 } = err;
-//     if (!err.message) err.message = 'Oh No, Something Went Wrong!'
-//     res.status(statusCode).render('error', { err })
-// })
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+    res.status(statusCode).render('error', { err })
+})
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
--- Login/models/user.js
+++ Login/models/user.js
@@ -3,6 +3,11 @@ const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
 
 const UserSchema = new Schema({
+    fullname:{
+        type: String,
+        required: true,
+        unique: true
+    },
     email: {
         type: String,
         required: true,
