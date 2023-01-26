52bcd46ba8b8fd19f27679941218b4467a455a1f
AnushreeDere <114243957+AnushreeDere@users.noreply.github.com>
2023-01-26 15:02:15 +0530

    Improving Search Algorithm
    Improving Search Algorithm

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
 #from ipynb.fs.full.Search_algorithm import searching
-from model import searching, drugid, drugcondition, drugrating
+from model import searching11, drugid, drugcondition, drugrating
 
 # Create flask app
 app = Flask(__name__)
 #model = pickle.load(open("modelnew.pkl", "rb"))
 
-#print(searching("pain"))
+print(searching11("cancer"))
 
 @app.route('/')
 def home():
@@ -17,7 +17,7 @@ def home():
 @app.route('/search',methods=['GET','POST'])
 def search():
     data=request.form.get("data")
-    med=searching(data)
+    med=searching11(data)
     id=drugid(med)
     condition=drugcondition(med)
     rating=drugrating(med)
