be241f9f25f22ad236059a1f02b920999b5afe3f
roze-ferns <rozeeta1107@gmail.com>
2023-01-28 01:28:00 +0530

    added logout for ejs files

--- ML + WEB FINAL/ML+WEB/app.js
+++ ML + WEB FINAL/ML+WEB/app.js
@@ -75,6 +75,7 @@ app.use(flash());
 // Global variables
 
 app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
   res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
   res.locals.error = req.flash('error');
