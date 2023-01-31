d96688431e36aded084af23947019402806a2e59
roze-ferns <rozeeta1107@gmail.com>
2023-01-31 23:31:05 +0530

    added .ejs files

--- ML + WEB FINAL/ML+WEB/app.js
+++ ML + WEB FINAL/ML+WEB/app.js
@@ -45,6 +45,7 @@ app.use(expressLayouts); //order imp
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')))
+// app.use(express.static(path.join(__dirname, 'static')))
 // app.use(express.static(__dirname + '/public'));
 // app.use( express.static( "public" ) );
 
